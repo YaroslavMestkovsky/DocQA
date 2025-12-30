e9fb18595ac37effe90b2860893e24f249449418
Yaroslav Mestkovsky <mestkovskij@obt-vlg.ru>
2025-12-30 10:57:40 +0300

    # 7 Правки

--- src/helpers/health_checks.py
+++ src/helpers/health_checks.py
@@ -1,4 +1,5 @@
-from src.helpers.models_management import check_models
+from src.helpers.models_management import check_ollama_models
+from src.helpers.models_management import check_embedding_model
 from src.helpers.qdrant_management import check_collections
 
 
@@ -7,13 +8,14 @@ def create_health_report():
 
     1. qdrant и его коллекции
     2. ollama и её модели
+    3. модели эмбединга
     """
 
     report = {}
 
     missing_qdrant_collections = check_collections()
-    missing_ollama_models = check_models()
-
+    missing_ollama_models = check_ollama_models()
+    missing_embedding_models = check_embedding_model()
 
     if missing_qdrant_collections:
         report["qdrant"] = "Missing collections ❌"
@@ -25,5 +27,10 @@ def create_health_report():
         report["ollama"] = "Missing models ❌"
     else:
         report["ollama"] = "OK ✅"
+
+    if missing_embedding_models:
+        report["embedding"] = "Missing models ❌"
+    else:
+        report["embedding"] = "OK ✅"
 
     return report
--- src/helpers/models_management.py
+++ src/helpers/models_management.py
@@ -72,7 +72,7 @@ def pull_ollama_model(name):
 def get_embedding_model() -> SentenceTransformer:
     """Возвращает единственный экземпляр модели эмбеддингов (singleton)."""
 
-    model_name = embedding_config.embedding
+    model_name = embedding_config.models.embedding
     model = SentenceTransformer(model_name)
 
     return model
@@ -85,14 +85,21 @@ def check_embedding_model():
 
     return {"embedding_model": "Ok" if cache_info.hits > 0 else "Not uploaded"}
 
-    # response = requests.get(ollama_config.ollama.models_list_url)
-    #
-    # if response.status_code == requests.codes.ok:
-    #     result = response.json()
-    # else:
-    #     result = {"error": f"Ошибка {response.status_code}: {response.text}"}
-    #
-    # return result
+
+def pull_embedding_model():
+    """Загрузка модели эмбеддингов."""
+
+    result = {}
+    cache_info = get_embedding_model.cache_info()
+
+    if cache_info.hits > 0:
+        result["success"] = "Модель уже загружена"
+    else:
+        get_embedding_model()
+        result["success"] = "Модель успешно загружена"
+
+    return result
+
 
 if __name__ == "__main__":
-    check_embedding_model()
+    get_embedding_model()
